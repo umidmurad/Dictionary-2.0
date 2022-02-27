6c91238b66210186bd862c32060b5742045f0e89
Umid Muradli <umid.murad@hotmail.com>
2022-02-27 13:24:33 -0800

    Moved it

---
+++ Fileloader.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Umid Muradli on 2/27/22.
+//
+
+#include <iostream>
+#include <fstream>
+#include "sstream"
+#include <vector>
+#include "algorithm"
+
+using namespace std;
+string reWriterSorter(string);
+string reWriterInput(string);
+vector< pair<string, pair<string, string> > > myvec;
+void FileLoader() {
+    string line, opener ="/Users/umidmuradli/Documents/GitHub/Dictionary-2.0/cmake-build-debug/Data.CS.SFSU.txt";
+    fstream FileReader;
+    FileReader.open(opener, ios::in);
+    cout <<"! Opening data file... " << opener <<endl;
+    while (!FileReader.is_open()) {
+        cout <<"<!>ERROR<!> ===> File could not be opened.\n<!>ERROR<!> ===> Provided file path: " << opener <<endl;
+        cout <<"<!>Enter the CORRECT data file path: ";
+        getline(cin, opener);
+        FileReader.open(opener, ios::in); //tries until file is open.
+    }
+    //once file is open it starts to load the data.
+    if (FileReader.is_open()) {
+        cout <<"! Loading data...\n";
+        while (getline(FileReader, line)) {
+            string word, pos, def, posAndDef;
+            char pipe = '|', arrow = ' -';
+            size_t defsAmount = count(line.begin(), line.end(), pipe); //counts the "|"
+            word = line.substr(0, line.find(pipe));
+            word = reWriterSorter(word);
+            cout << "\n" << word << "-----------------------------" << endl;
+            line.erase(0, line.find(pipe) + 1);
+            stringstream pipeSep(line);
+
+            for (int i = 0; i < defsAmount; i++) {
+                getline(pipeSep, posAndDef, pipe); // pipe = "|"
+
+                pos = posAndDef.substr(0, posAndDef.find(arrow) - 1);
+                cout << pos << ": ";
+                def = posAndDef.erase(0, posAndDef.find(arrow) + 5);
+                cout << def << endl;
+                def = reWriterSorter(def);
+
+                myvec.push_back(make_pair(word, make_pair(pos, def)));   // new
+            }
+        }
+        sort(myvec.begin(), myvec.end()); //sorts alphabetically.
+        cout <<"! Loading completed...\n";
+    }
+    FileReader.close();
+
+}
+
+string reWriterSorter(string str){
+    str[0] = toupper(str[0]);
+    return str;
+}
+string reWriterInput(string str){
+    for(int i = 0; i < str.size(); i++)
+        str[i] = tolower(str[i]);
+    str[0] = toupper(str[0]);
+    return str;
+}
--- main.cpp
+++ main.cpp
@@ -1,91 +1,30 @@
 // Created by Umid Muradli on 2/23/22.
 //
 #include <iostream>
-#include <fstream>
 #include <string>
-#include "sstream"
-#include <vector>
 #include "algorithm"
+#include "Fileloader.cpp"
 using namespace std;
 
 int main() {
-    string reWriterSorter(string);
-    string reWriterInput(string);
-    string line;
-    fstream FileReader;
-    vector< pair<string, pair<string, string> > > myvec;
-    FileReader.open("../Data.CS.SFSU.txt", ios::in);
-    if (FileReader.is_open()) {
-        while (getline(FileReader, line)) {
-            string word, pos, def, posAndDef;
-            char pipe = '|';
-            char arrow = ' -';
-            size_t defsAmount = count(line.begin(), line.end(), pipe); //moved here
-            word = line.substr(0, line.find('|'));
-            word = reWriterSorter(word);
-            cout << "\n" << word << "-----------------------------" << endl;
-            line.erase(0, line.find('|') + 1);
-            stringstream pipeSep(line);
-
-            //size_t defsAmount = count(line.begin(), line.end(), arrow); moved this up, and it counts pipe, instead of arrow
-            for (int i = 0; i < defsAmount; i++) {
-                getline(pipeSep, posAndDef, pipe); // pipe = "|"
-
-                pos = posAndDef.substr(0, posAndDef.find(arrow) - 1);
-                cout << pos << ": ";
-                def = posAndDef.erase(0, posAndDef.find(arrow) + 5);
-                cout << def << endl;
-                def = reWriterSorter(def);
-
-                myvec.push_back(make_pair(word, make_pair(pos,def)));   // new
-            }
-        }
-        sort(myvec.begin(), myvec.end()); //new
-
-    }
-
-    else { //if cant open the file
-        cout << "Error opening the file.";
-    }
-
-    FileReader.close();
-
+    FileLoader(); //loads the data
     //new after this
     string input;
     while(input != "!q") {
-
         cout << "\n\nType something here:   ";
         cin >> input;
         input = reWriterInput(input);
-        cout <<"\n|\n";
+        cout <<"\n|";
         for (int i = 0; i < myvec.size(); i++) {
 
             while (input == myvec[i].first) {
 
                 cout  <<endl << myvec[i].first << " [" << myvec[i].second.first
-                     << "]  : " << myvec[i].second.second <<endl ;
+                     << "]  : " << myvec[i].second.second  ;
                 i++;
             }
-
         }
-        cout <<"\n|\n";
-
+        cout <<"\n|";
     }
-
-
-
-
-
-
     return 0;
-}
-string reWriterSorter(string str){
-    str[0] = toupper(str[0]);
-    return str;
-}
-string reWriterInput(string str){
-    for(int i = 0; i < str.size(); i++)
-        str[i] = tolower(str[i]);
-    str[0] = toupper(str[0]);
-    return str;
 }
