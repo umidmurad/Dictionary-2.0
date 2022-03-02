cb3b79247a2523e9c7612d54cafacf353c62f03b
Diego Flores <diegofloresflores2001@gmail.com>
2022-03-01 16:27:54 -0800

    .

--- Printer.cpp
+++ Printer.cpp
@@ -2,7 +2,7 @@
 // Created by Umid Muradli on 3/1/22.
 //
 
-bool wordprinter(string);
+void wordprinter(string);
 void inputSep(string);
 void posprinter(vector<string>);
 void filler();
@@ -12,25 +12,6 @@ vector<string> spchType2;
 vector<string> distinct3;
 vector<string> reverse4;
 
-void errorChecker(string input, int caseNumber){
-    switch (caseNumber) {
-        case 2: cout<< endl << "<The entered" << caseNumber << "nd parameter'" << input << " is NOT a part of speech.>" << endl;
-        cout << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
-        cout << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-        cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-        cout << "<The " << caseNumber <<"nd parameter should be a part of speech or 'distinct' or 'reverse'.>" << endl << "|" << endl;
-            break;
-        case 3: cout << endl << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
-            cout <<"<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-            cout << "<The " << caseNumber <<"nd parameter should be 'distinct' or 'reverse'.>" << endl << "|" << endl;
-            break;
-        case 4: cout << endl << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-            cout << "<The " << caseNumber <<"nd parameter should be 'reverse'.>" << endl << "|" << endl;
-            break;
-    }
-}
 void filler(){
     spchType2.push_back("verb");
     spchType2.push_back("noun");
@@ -68,7 +49,7 @@ void inputSep(string input){
     }
 }
 
-bool wordprinter(string input){
+void wordprinter(string input){
     //we need error function when word doesn't exist.
     bool check = false;
     cout << "|" << endl;
@@ -79,12 +60,36 @@ bool wordprinter(string input){
             check =true;
         }
     }cout << "|\n" ;
-    if(!check) notAvailable();
- return check;
+    if(!check) notAvailable(); // if word not printed (check = false), not Available is called
 }
 void notAvailable(){
     cout <<"\n<NOT FOUND> To be considered for the next release. Thank you.";
 }
+
+void errorChecker(string input, int caseNumber){
+    switch (caseNumber) {
+        case 2: cout<< endl << "<The entered" << caseNumber << "nd parameter'" << input << " is NOT a part of speech.>" << endl;
+            cout << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
+            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << "<The " << caseNumber <<"nd parameter should be a part of speech or 'distinct' or 'reverse'.>" << endl << "|" << endl;
+            break;
+        case 3: cout << endl << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
+            cout <<"<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << "<The " << caseNumber <<"nd parameter should be 'distinct' or 'reverse'.>" << endl << "|" << endl;
+            break;
+        case 4: cout << endl << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << "<The " << caseNumber <<"nd parameter should be 'reverse'.>" << endl << "|" << endl;
+            break;
+    }
+}
+
+//existOrNot can be reused for reverse and distinct key option
+// in the case of: Book noun reverse, do: existOrNot(input *which holds reverse* , *whatever list you want to check if
+// input exists in, for third position do distinct3*)
+
 bool existOrNot(string input, vector<string> listToCheck){
     for(string option: listToCheck)
         if (option == input)
@@ -104,9 +109,9 @@ void posprinter(vector<string> optionHolder)
 
     }
     cout<< "|";
-    if(!check) return;
+    if(!check) return; // if the word and pos was founded and printed, do not try to check for errors (line 111-113), just return back to main
     if(!existOrNot(optionHolder[1], spchType2)) // if input is not in the list spchType2
-        errorChecker(optionHolder[1], 2);
+        errorChecker(optionHolder[1], 2); // 2 is for second position is invalid.
     wordprinter(optionHolder[0]); // wordPrinter will attempt to print word, if not in txt file, it calls notAvailable()
 
 
