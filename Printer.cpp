98a29f003a4dbafbf7303406e77fe59282a35795
Umid Muradli <umid.murad@hotmail.com>
2022-03-01 22:57:21 -0800

    Added reverse & distinct function

--- Printer.cpp
+++ Printer.cpp
@@ -5,52 +5,18 @@
 void wordprinter(string);
 void inputSep(string);
 void posprinter(vector<string>);
+void distincthelper(vector<string>);
+void reversehelper(vector <string>);
 void filler();
 void errorChecker(string, int);
 void notAvailable();
+bool existOrNot(string, vector<string>);
+vector< pair<string, pair<string, string> > > tempvec;
 vector<string> spchType2;
 vector<string> distinct3;
 vector<string> reverse4;
 
 
-
-void inputSep(string input){
-    vector<string> optionHolder; //declare new vector
-    string temp = input, option; //temp holds the input
-    input.append(" "); // If I do not add a space, it will not read the last thing inputted
-    stringstream line(input);
-    int numberOfSpaces = count(input.begin(), input.end(), ' ');
-    for(int i = 0; i < numberOfSpaces; i++) {
-        getline(line, option, ' ');
-        if (option == "") // If Extra Space is inputted skip to next word
-            continue;
-        optionHolder.push_back(option);
-    }
-    switch (numberOfSpaces) {
-        case 1: wordprinter(temp); break;
-        case 2: posprinter(optionHolder); break;
-        case 3: cout << "distinct, reverse" << endl; break;
-        case 4: cout << "reverse" << endl; break;
-        default: cout << "You have inputted more than 4 options" << endl;
-    }
-}
-
-void wordprinter(string input){
-    //we need error function when word doesn't exist.
-    bool check = false;
-    cout << "|" << endl;
-    for (int i = 0; i < myvec.size(); i++) {
-        if (input == myvec[i].first) {
-            cout << myvec[i].first << " [" << myvec[i].second.first
-                  << "]  : " << myvec[i].second.second<< endl;
-            check =true;
-        }
-    }cout << "|\n" ;
-    if(!check) notAvailable(); // if word not printed (check = false), not Available is called
-}
-void notAvailable(){
-    cout <<"\n<NOT FOUND> To be considered for the next release. Thank you.";
-}
 void filler(){
     spchType2.push_back("verb");
     spchType2.push_back("noun");
@@ -67,29 +33,131 @@ void filler(){
     reverse4.push_back("reverse");
 }
 
-void errorChecker(string input, int caseNumber){
-    switch (caseNumber) {
-        case 2: cout<< endl << "<The entered" << caseNumber << "nd parameter'" << input << " is NOT a part of speech.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-            cout << "<The " << caseNumber <<"nd parameter should be a part of speech or 'distinct' or 'reverse'.>" << endl << "|" << endl;
-            break;
-        case 3: cout << endl << "<The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
-            cout <<"<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-            cout << "<The " << caseNumber <<"nd parameter should be 'distinct' or 'reverse'.>" << endl << "|" << endl;
-            break;
-        case 4: cout << endl << "<The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
-            cout << "<The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
-            cout << "<The " << caseNumber <<"nd parameter should be 'reverse'.>" << endl << "|" << endl;
-            break;
+void inputSep(string input){
+    vector<string> optionHolder; //declare new vector
+    string temp = input, option; //temp holds the input
+    input.append(" "); // If I do not add a space, it will not read the last thing inputted
+    stringstream line(input);
+    int numberOfSpaces = count(input.begin(), input.end(), ' ');
+    for(int i = 0; i < numberOfSpaces; i++) {
+        getline(line, option, ' ');
+        if (option == "") // If Extra Space is inputted skip to next word
+            continue;
+        optionHolder.push_back(option);
+    }
+    switch (numberOfSpaces) {
+        case 1: wordprinter(temp); break;
+        case 2: reversehelper(optionHolder); break; //posprinter(optionHolder); distincthelper(optionHolder);
+        case 3: cout << "distinct, reverse" << endl; break;
+        case 4: cout << "reverse" << endl; break;
+        default: cout << "You have inputted more than 4 options" << endl;
     }
 }
+
+void wordprinter(string input){
+    //we need error function when word doesn't exist.
+    bool check = false;
+    cout << "|" << endl;
+    for (int i = 0; i < myvec.size(); i++) {
+        if (input == myvec[i].first) {
+            cout  <<" " << myvec[i].first << " [" << myvec[i].second.first
+                  << "]  : " << myvec[i].second.second <<endl;
+            check =true;
+        }
+    }
+    if(!check) notAvailable(); // if word not printed (check = false), not Available is called
+    cout << "|\n" ;
+}
+
 
 //existOrNot can be reused for reverse and distinct key option
 // in the case of: Book noun reverse, do: existOrNot(input *which holds reverse* , *whatever list you want to check if
 // input exists in, for third position do distinct3*)
+
+
+void posprinter(vector<string> optionHolder)
+{       //this function needs another function to check if the pos actually is not random word.
+    bool check= true;
+    cout << "|\n";
+
+    for (int i = 0; i < myvec.size(); i++) {
+        if (optionHolder[0] == myvec[i].first && optionHolder[1] ==myvec[i].second.first) {
+            cout<<" " << myvec[i].first << " [" << myvec[i].second.first
+                  << "]  : " << myvec[i].second.second << endl;
+            check = false;
+        }
+
+    }
+     // if the word and pos was founded and printed, do not try to check for errors (line 114-116), just return back to main
+
+    if(!check) { cout << "|"; return; }// if the word and pos was founded and printed, do not try to check for errors (line 114-116), just return back to main
+    if(!existOrNot(optionHolder[1], spchType2)) // if input is not in the list spchType2
+        errorChecker(optionHolder[1], 2); // 2 is for second position is invalid.
+    wordprinter(optionHolder[0]); // wordPrinter will attempt to print word, if not in txt file, it calls notAvailable()
+}
+
+
+
+//___________________________________________________
+//DISTINCT
+void distincthelper(vector<string> optionHolder){
+    vector<pair<string, pair<string, string> > > tempvec = myvec;
+    int j=0;
+    if(tempvec.size()>1) {
+        for (int i = 1; i < tempvec.size(); i++) {
+            if ((tempvec[j].first ==tempvec[i].first) && (tempvec[j].second.first==tempvec[i].second.first)&&(tempvec[j].second.second==tempvec[i].second.second)){
+                tempvec.erase(tempvec.begin() + j);
+                i--;
+                continue;
+            }
+            j++;
+        }
+    }
+
+    for (int i = 0; i < tempvec.size(); i++) {
+        if (optionHolder[0] == tempvec[i].first) {
+            cout<<" " << tempvec[i].first << " [" << tempvec[i].second.first
+                << "]  : " << tempvec[i].second.second << endl;
+
+        }
+    }
+}
+
+//REVERSE
+void reversehelper(vector <string> optionHolder){
+    vector<pair<string, pair<string, string> > > tempvec = myvec;
+    reverse(tempvec.begin(),tempvec.end());
+    for (int i = 0; i < tempvec.size(); i++) {
+        if (optionHolder[0] == tempvec[i].first) {
+            cout<<" " << tempvec[i].first << " [" << tempvec[i].second.first
+                << "]  : " << tempvec[i].second.second << endl;
+
+        }
+    }
+}
+
+
+
+//___________________________________________________
+void errorChecker(string input, int caseNumber){
+    switch (caseNumber) {
+        case 2: cout << " <The entered " << caseNumber << "nd parameter '" << input << "' is NOT a part of speech.>" << endl;
+            cout << " <The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
+            cout << " <The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << " <The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << " <The " << caseNumber <<"nd parameter should be a part of speech or 'distinct' or 'reverse'.>" << endl << "|" << endl;
+            break;
+        case 3: cout  << " <The entered " << caseNumber << "nd parameter '" + input << "' is NOT 'distinct'.>" << endl;
+            cout <<" <The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << " <The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << "<The " << caseNumber <<"nd parameter should be 'distinct' or 'reverse'.>" << endl << "|" << endl;
+            break;
+        case 4: cout  << " <The entered " << caseNumber << "nd parameter '" << input << "' is NOT 'reverse'.>" << endl;
+            cout << " <The entered " << caseNumber << "nd parameter '" << input << "' was disregarded.>" << endl;
+            cout << " <The " << caseNumber <<"nd parameter should be 'reverse'.>" << endl << "|" << endl;
+            break;
+    }
+}
 
 bool existOrNot(string input, vector<string> listToCheck){
     for(string option: listToCheck)
@@ -97,61 +165,6 @@ bool existOrNot(string input, vector<string> listToCheck){
             return true;
     return false;
 }
-void posprinter(vector<string> optionHolder)
-{       //this function needs another function to check if the pos actually is not random word.
-    bool check= true;
-    cout << "|"<< endl;
-    for (int i = 0; i < myvec.size(); i++) {
-        if (optionHolder[0] == myvec[i].first && optionHolder[1] ==myvec[i].second.first) {
-            cout<< myvec[i].first << " [" << myvec[i].second.first
-                  << "]  : " << myvec[i].second.second << endl;
-            check = false;
-        }
-
-    }
-    cout<< "|";
-    if(!check) return; // if the word and pos was founded and printed, do not try to check for errors (line 114-116), just return back to main
-    if(!existOrNot(optionHolder[1], spchType2)) // if input is not in the list spchType2
-        errorChecker(optionHolder[1], 2); // 2 is for second position is invalid.
-    wordprinter(optionHolder[0]); // wordPrinter will attempt to print word, if not in txt file, it calls notAvailable()
-
-
-    cout << "|" << endl;
-    //if(check){
-        //errorChecker(optionHolder[1],2); // 2 means, pos is wrong input
-   // } //checks if function exists
+void notAvailable(){
+    cout <<" <NOT FOUND> To be considered for the next release. Thank you." <<endl;
 }
-
-
-
-
-
-//vector<string> inputSep(string input){
-//    input.append(" "); // If I do not add a space, it will not read the last thing inputted
-//    vector<string> optionHolder;
-//    stringstream line(input);
-//    string option;
-//    size_t numberOfSpaces = count(input.begin(), input.end(), ' ');
-//    for(size_t i = 0; i < numberOfSpaces; i++) {
-//        getline(line, option, ' ');
-//        if(option == "") // If Extra Space is inputted skip to next word
-//            continue;
-//        optionHolder.push_back(option);
-//    }
-//    /*for(string temp : optionHolder)
-//        cout << temp << endl;*/
-//return optionHolder;
-//}
-
-
-//void caseChooser(string input, ){
-//    vector<string> optionHolder = inputSep(input);
-//    //vector<string> optionHolder = inputSep(input);
-//    switch (optionHolder.size()) {
-//        case 1: wordprinter(input); break;
-//        case 2: cout << "print lookUp, distinct, reverse" << endl; break;
-//        case 3: cout << "distinct, reverse" << endl; break;
-//        case 4: cout << "reverse" << endl; break;
-//        default: cout << "You have inputted more than 4 options" << endl;
-//    }
-//}
